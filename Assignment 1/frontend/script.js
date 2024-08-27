c3174baf11d2e66dcd326637a11511a6fd63a2d7
Irwindeep <b22ai022@iitj.ac.in>
2024-08-28 00:19:43 +0530

    Added server.js

--- Assignment 1/frontend/script.js
+++ Assignment 1/frontend/script.js
@@ -2,18 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentLocation = location.href;
     const menuItems = document.querySelectorAll('.nav-links a');
 
-    // Set the active class on the initial load
     menuItems.forEach(menuItem => {
         if (menuItem.href === currentLocation) {
             menuItem.classList.add("active");
         }
 
-        // Add click event listener to each menu item
         menuItem.addEventListener('click', () => {
-            // Remove the active class from all menu items
             menuItems.forEach(item => item.classList.remove("active"));
-
-            // Add the active class to the clicked menu item
             menuItem.classList.add("active");
         });
     });
