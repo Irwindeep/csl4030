3f1d4583a4ae20b24ece3b1e3d43b410c4401154
Irwindeep <b22ai022@iitj.ac.in>
2024-09-13 11:45:32 +0530

    Finalised

--- Assignment 1/HotelManagement/app.py
+++ Assignment 1/HotelManagement/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session # type: ignore
+from flask import Flask, render_template, request, redirect, url_for, session
 import MySQLdb
 import os
 import hashlib
@@ -55,7 +55,10 @@ def report():
         cursor.execute("SELECT * FROM bookings")
         bookings = cursor.fetchall()
 
-        cursor.execute("SELECT SUM(r.price * DATEDIFF(b.booking_till, b.booking_date)) AS total_revenue, (COUNT(DISTINCT b.room_no) / (SELECT COUNT(room_no) FROM rooms)) * 100 AS occupancy_percentage FROM  bookings b JOIN rooms r ON b.room_no = r.room_no")
+        cursor.execute('''SELECT 
+                        (SELECT SUM(amount) FROM payments) AS total_payments,
+                        (SELECT COUNT(*) FROM rooms WHERE status = 'Booked')/ (SELECT COUNT(*) FROM rooms)*100 AS booked_ratio;
+                    ''')
         reports = cursor.fetchone()
         report_ = []
         for rep in reports: report_.append(str(rep))
@@ -74,30 +77,21 @@ def room_management():
         room_info = cursor.fetchone()
 
         booking_info = None
-        if room_info and room_info[1]:  # Assuming room_info[1] is the status column
+        if room_info:
             cursor.execute("SELECT * FROM bookings WHERE room_no = %s", (room_number,))
             booking_info = cursor.fetchone()
-
-        if room_info:
+            
             return render_template('room_management.html', room_info=room_info, booking_info=booking_info, username=session['username'])
         else:
             error = f"No information found for room number {room_number}"
             return render_template('room_management.html', error=error, username=session['username'])
     return render_template('room_management.html', username=session['username'])
 
-
-@app.route('/booking')
-def booking():
-    return render_template('booking.html')
-
-@app.route('/payment')
-def payment():
-    return render_template('payment.html')
-
 @app.route('/logout')
 def logout():
-    return render_template('logout.html')
+    session.clear()
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
